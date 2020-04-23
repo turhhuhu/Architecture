f84cb26a325d8f7bf357a984b9fbc05ff26c0db4
turhhuhu <turhhuhu@gmail.com>
2020-04-23 22:14:16 +0300

    sheker

--- Assignment1/task2/asm_task2Assignment1.s
+++ Assignment1/task2/asm_task2Assignment1.s
@@ -1,14 +1,18 @@
+section .data
+	index: db 0
+	
 section	.rodata			; we define (global) read-only variables in .rodata section
 	format_string: db "%s", 10, 0	; format string
 	format: db "%d", 10, 0
+	hexStr: db "0123456789ABCDEF", 0 
 
 section .bss			; we define (global) uninitialized variables in .bss section
-	an: resb 12		; enough to store integer in [-2,147,483,648 (-2^31) : 2,147,483,647 (2^31-1)]
+	an: resb 12	; enough to store integer in [-2,147,483,648 (-2^31) : 2,147,483,647 (2^31-1)]
 
 section .text
 	global convertor
 	extern printf
-
+	
 convertor:
 	push ebp
 	mov ebp, esp	
@@ -16,29 +20,47 @@ convertor:
 
 	mov ecx, dword [ebp+8]	; get function argument (pointer to string)
 	mov eax, 0
+	mov ebx, 0
 	for_loop:
 		cmp byte [ecx], 10
 		jz end_loop
 		cmp byte [ecx], 0
 		jz end_loop
-		mov dword ebx, [ecx]
-		sub ebx, '0'
+		mov bl, [ecx]
+		sub bl, '0'
 		imul eax, 10
 		add eax, ebx
+		inc ecx
 		jmp for_loop
 	end_loop:
-	push eax
-	push format
-	call printf
-	add esp, 8
-	; your code comes here...
 
+	mov ebx ,0 ;temp
+	mov edx, 0 ;index an
+	mov ecx, 0
+	for_loop_hexa:
+		mov ecx, hexStr
+		cmp byte [index], 8
+		jge end_loop_hexa
+		mov bl, al
+		and bl, 00001111b
+		cmp bl, 0
+		jz end_loop_hexa
+		add ecx, ebx
+		mov ecx, [ecx]
+		mov edx, [index]
+		add edx, an
+		mov [edx], ecx
+		shr eax, 4
+		inc byte [index]
+		jmp for_loop_hexa
+	end_loop_hexa:
+	mov byte [edx + 1], 0 
 	push an			; call printf with 2 arguments -  
 	push format_string	; pointer to str and pointer to format string
 	call printf
 	add esp, 8		; clean up stack after call
-	
-	popad			
+
+	popad
 	mov esp, ebp	
 	pop ebp
 	ret
--- Assignment1/task2/main_task2Assignment1.c
+++ Assignment1/task2/main_task2Assignment1.c
@@ -5,26 +5,9 @@ extern int convertor(char* buf);
 
 int main(int argc, char** argv)
 {
-  char buf[MAX_LEN];
+	char buf[MAX_LEN];
  
-  fgets(buf, MAX_LEN, stdin);		/* get user input string */ 
-  printf("%s", buf);
-  convertor(buf);			/* call your assembly function */
-
-  return 0;
-
-  int base = 1;
-  int sum = 0;
-  while(8)
-  {
-    while(4)
-    {
-      1001 >> 0100 cf = 1 
-      cf = shift_right(eax);
-      sum += cf * base;
-      base++
-    }
-    //do something with sum
-    base = 1;
-  }
-}
+	fgets(buf, MAX_LEN, stdin);		/* get user input string */ 
+	convertor(buf);			/* call your assembly function */
+	return 0; 
+}           								
